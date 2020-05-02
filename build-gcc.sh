89b33c75ff7a866257978b1aa2a4dc795c9dfb9a
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-05-01 21:46:56 -0500

    fix build scripts

--- build-gcc.sh
+++ build-gcc.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-VVCTRE_VERSION=28.x.x
-
-g++ -o plugin.vvctre-$VVCTRE_VERSION.so -shared plugin.cpp
+g++ -o plugin.so -shared plugin.cpp
--- build-visual-studio-2019-build-tools.cmd
+++ build-visual-studio-2019-build-tools.cmd
@@ -1,12 +1,10 @@
 @echo off
 
-set VVCTRE_VERSION=28.x.x
-
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-cl /LD /Fe:plugin.vvctre-%VVCTRE_VERSION%.dll plugin.cpp
+cl /LD plugin.cpp
 
-del plugin.vvctre-%VVCTRE_VERSION%.lib
-del plugin.vvctre-%VVCTRE_VERSION%.exp
+del plugin.lib
+del plugin.exp
 del plugin.obj
 
 echo Useless files deleted
